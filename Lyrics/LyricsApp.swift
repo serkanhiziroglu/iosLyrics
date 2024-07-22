98a6bef1b1fe0cca7c6268bfa8d951fcc413cb8c
Serkan Hiziroglu <71054911+serkanhiziroglu@users.noreply.github.com>
2024-07-22 14:00:37 -0700

    Initial Commit

---
+++ Lyrics/LyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsApp.swift
+//  Lyrics
+//
+//  Created by Serkan MP on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
